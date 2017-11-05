051630adc462199a1455e1e8ff39f44966b4d926
xtlsoft <xtlsoft@126.com>
2017-11-05 09:03:13 +0800

    [+] Added Different Component

---
+++ src/Components/Different.php
@@ -0,0 +1,22 @@
+<?php
+    /**
+     * NonDB - A NoSQL Database
+     * 
+     * This Program licensed under MIT.
+     * Use it under the Law and the License.
+     * 
+     * @author Tianle Xu<xtl@xtlsoft.top>
+     * @package NonDB
+     * @license MIT
+     * @category database
+     * @link https://github.com/xtlsoft/NonDB/
+     * 
+     */
+
+    namespace NonDB\Components;
+
+    class Different {
+
+        
+
+    }
